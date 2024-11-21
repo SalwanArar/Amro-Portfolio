14ada33dc1cde02ccae98d44396ad62f62ace41d
Salwan Arar <salwan.arar@gmail.com>
2024-11-21 23:41:45 +0200

    Improve image load

--- src/pages/ProjectDetailsPage/index.jsx
+++ src/pages/ProjectDetailsPage/index.jsx
@@ -15,37 +15,18 @@ function YoutubeCard(props) {
       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
       referrerPolicy="strict-origin-when-cross-origin"
       allowFullScreen
+      loading="lazy"
     ></iframe>
   );
 }
 
-// function paddingNumber(id) {
-//   return String(id).padStart(2, "0");
-// }
-
-// const getImages = (projectId, num) => {
-//   const images = [];
-//   for (var i = 2; i <= num; i++) {
-//     const imageSrc = require(`../../assets/images/${paddingNumber(
-//       projectId
-//     )}/${paddingNumber(i)}.jpg`);
-//     images.push(imageSrc);
-//   }
-//   return images;
-// };
-
 function ProjectDetailsPage() {
   const { projectId } = useParams();
-  // const imageSrc = require(`../../assets/images/${paddingNumber(
-  //   projectId
-  // )}/${paddingNumber(1)}.jpg`);
   const project = projects.find((proj) => String(proj.id) === projectId);
-  // const images = getImages(projectId, project.numImg);
 
   return (
     <div className="project-details">
       <h1>{project.name}</h1>
-      {/* <img src={imageSrc} alt={`Image: ${project.name}`} /> */}
       {project.children.map((child, idx) => {
         if (child.type === "youtube")
           return (
@@ -56,22 +37,17 @@ function ProjectDetailsPage() {
             />
           );
         else {
-          return <img key={idx} src={child.src} alt={`image-${idx}`} />;
+          return (
+            <img
+              key={idx}
+              src={child.src}
+              alt={`image-${idx}`}
+              loading="lazy"
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
+          );
         }
       })}
-      {/* {project.youtube.map((src, idx) => (
-        <YoutubeCard
-          key={idx}
-          src={src}
-          alt={`YouTube: ${project.name}-${idx}`}
-        />
-      ))}
-      {images.map((image, idx) => (
-        <img key={idx} src={image} alt={`image-${idx}`} />
-      ))}
-      {project.gifs.map((gif, idx) => (
-        <img key={idx} src={gif} alt={`gif-${idx}`} />
-      ))} */}
     </div>
   );
 }
