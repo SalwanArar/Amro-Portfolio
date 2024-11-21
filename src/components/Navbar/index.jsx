ba12111081ab2559cac2bf84ccebb0379d136957
Salwan Arar <salwan.arar@gmail.com>
2024-11-21 20:34:42 +0200

    Modify Contacts

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,6 +3,7 @@ import {
   faInstagram,
   faLinkedinIn,
   faWhatsapp,
+  faBehance,
 } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { NavLink } from "react-router-dom";
@@ -23,7 +24,24 @@ const links = [
     text: "contact",
   },
 ];
-
+const icons = [
+  {
+    href: "https://linkedin.com/in/amro-ghrzelddin/",
+    icon: faLinkedinIn,
+  },
+  {
+    href: "https://www.instagram.com/amrogharz/",
+    icon: faInstagram,
+  },
+  {
+    href: "https://api.whatsapp.com/send?phone=971509386638",
+    icon: faWhatsapp,
+  },
+  {
+    href: "https://www.behance.net/2089b8ff",
+    icon: faBehance,
+  },
+];
 function Navbar(props) {
   const closeNavbar = () => props.setToggle(false);
   return (
@@ -42,9 +60,11 @@ function Navbar(props) {
           </ul>
         </nav>
         <div className="contact-bar">
-          <FontAwesomeIcon icon={faLinkedinIn} />
-          <FontAwesomeIcon icon={faInstagram} />
-          <FontAwesomeIcon icon={faWhatsapp} />
+          {icons.map((icon, index) => (
+            <a href={icon.href} key={index} target="_blank" rel="noreferrer">
+              <FontAwesomeIcon icon={icon.icon} color="#f5f5f7" />
+            </a>
+          ))}
         </div>
       </div>
     </header>
